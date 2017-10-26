3a31d7b535f00101b7037ee908cd2da2d50c81cb
zhan <390961827@qq.com>
2017-10-26 18:02:30 +0800

    修复一些问题

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -17,11 +17,13 @@ class WechatController extends Controller
     {
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $app = app('wechat.official_account');
-        $app->server->push(function($message){
+        $wechat = app('wechat');
+        $wechat->server->setMessageHandler(function($message){
             return "欢迎关注 overtrue！";
         });
 
-        return $app->server->serve();
+        Log::info('return response.');
+
+        return $wechat->server->serve();
     }
 }
